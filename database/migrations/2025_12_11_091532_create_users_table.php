df3d1f2bb981c95cc8ba3827ed52934595428d28
junya-1 <junya.mi746@gmail.com>
2026-01-14 04:26:24 +0900

    usersテーブルの修正

--- database/migrations/2025_12_11_091532_create_users_table.php
+++ database/migrations/2025_12_11_091532_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('country_id')->default(0);
-            $table->string('email', 100)->unique();
-            $table->string('password', 100);
-            $table->integer('role')->default(0);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('role');
+            $table->integer('country_id')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2026_01_13_184400_add_del_flg_to_players_table.php
+++ database/migrations/2026_01_13_184400_add_del_flg_to_players_table.php
@@ -6,22 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('players', function (Blueprint $table) {
             $table->tinyInteger('del_flg')
                   ->default(0)
-                  ->after('weight')
                   ->comment('削除フラグ 0:有効 1:削除');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('players', function (Blueprint $table) {
